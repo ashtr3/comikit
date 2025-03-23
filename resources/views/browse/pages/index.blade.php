2996fa4c5d19b04a9fcd93e00bdba740008a1291
ashtr3 <ashley@ashtree.studio>
2025-03-22 23:07:43 -0400

    added expand/collapse all script to archive

--- resources/views/browse/pages/index.blade.php
+++ resources/views/browse/pages/index.blade.php
@@ -30,46 +30,24 @@
     </details>    
 @endforeach
 
+<script>
 
-{{-- <h1>Pages</h1>
+document.addEventListener('DOMContentLoaded', function () {
+    
+    document.getElementById('gallery-expand-btn').addEventListener('click', function() {
+        document.querySelectorAll('details').forEach(el => {
+            el.setAttribute('open', '');
+        });
+    });
 
-<table>
-    <thead>
-        <tr>
-            <th>Index</th>
-            <th>Page #</th>
-            <th>Page Name</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($groupedPages as $volumeId => $chapters)
-            <tr>
-                <th colspan="4">
-                    {{ $chapters->first()->first()->chapter->volume->display_name }}
-                </th>
-            </tr>
-            @foreach ($chapters as $chapterId => $pages)
-                <tr>
-                    <th colspan="4">
-                        {{ $pages->first()->chapter->display_name }}
-                    </th>
-                </tr>
-                @foreach ($pages as $page)
-                    <tr>
-                        <td>{{ $page->page_index }}</td>
-                        <td>{{ $page->page_number }}</td>
-                        <td>{{ $page->page_name }}</td>
-                        <td>
-                            <a href="{{ route('browse.page.view', $page) }}">
-                                <button>View</button>
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
-            @endforeach
-        @endforeach
-    </tbody>
-</table> --}}
+    document.getElementById('gallery-collapse-btn').addEventListener('click', function() {
+        document.querySelectorAll('details').forEach(el => {
+            el.removeAttribute('open');
+        });
+    });
+
+});
+
+</script>
 
 @endsection
